d12dbdc81948b075814b6fed071c40fb53723e2a
Matteo Panara <matteo.panara@gmail.com>
2022-02-15 11:00:26 +0100

    Rspec started

---
+++ spec/takamaru_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe(Takamaru) do
+  describe '.gem_version' do
+    it 'returns the version of the gem' do
+      v = described_class.gem_version
+      expect(v).to(be_a(::Gem::Version))
+      expect(v.to_s).to(eq(::PaperTrail::VERSION::STRING))
+    end
+  end
+end
