4653d97e6e6eeacebb962b9f0f0cfb0bda1ca9ef
Danilo Filippelli <d.filippelli@fit2you.it>
2024-04-12 14:48:18 +0200

    Fix: Naming

--- lib/app/jobs/takamaru/commit_log_miner_job.rb
+++ lib/app/jobs/takamaru/commit_log_miner_job.rb
@@ -3,7 +3,7 @@ module Takamaru
     queue_as :takamaru
 
     def perform(commit_log_id)
-      commit_log = Takaru::CommitLog.find(commit_log_id)
+      commit_log = Takamaru::CommitLog.find(commit_log_id)
       RabbitMq::Publisher.new(commit_log.exchange_name).publish(commit_log.payload.to_json)
       commit_log.destroy
     end
